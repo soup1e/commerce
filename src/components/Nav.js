89d2171115e9dd745ae28f5c3f0233a98e1b7196
Sam Adams <sam.adams7785@gmail.com>
2023-07-07 15:03:35 -0700

    nav changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -20,19 +20,27 @@ export default function Nav() {
       </div>
 
       <div className="flex-none gap-4 flex items-center">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-        </div>
         <button className="btn btn-square btn-ghost">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="w-6 h-6"
+            className="w-8 h-8"
+          >
+            <path d="M8.25 10.875a2.625 2.625 0 115.25 0 2.625 2.625 0 01-5.25 0z" />
+            <path
+              fillRule="evenodd"
+              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.125 4.5a4.125 4.125 0 102.338 7.524l2.007 2.006a.75.75 0 101.06-1.06l-2.006-2.007a4.125 4.125 0 00-3.399-6.463z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-8 h-8"
           >
             <path
               fillRule="evenodd"
@@ -47,7 +55,7 @@ export default function Nav() {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              className="w-6 h-6"
+              className="w-7 h-7"
             >
               <path
                 fillRule="evenodd"
@@ -66,16 +74,31 @@ export default function Nav() {
           onClick={toggleCart}
         >
           <div
-            className="sidebar fixed top-0 right-0 z-50 h-screen w-80 bg-dark"
+            className="sidebar fixed top-0 right-0 z-50 h-screen w-80  bg-dark"
             onClick={(e) => e.stopPropagation()}
           >
-            <button className="btn btn-square btn-ghost" onClick={toggleCart}>
-              Button
-            </button>
-            <ul className="p-4">
-              <span className="font-bold text-lg">Items: {itemsCount}</span>
-              <span className="text-info">Subtotal: $99</span>
-            </ul>
+            <div className="flex justify-between items-center p-2 border-b-2 border-gray-900">
+              <div className="inline-flex font-bold text-lg p-4 text-gray-200">
+                Cart: {itemsCount}
+              </div>
+              <button
+                className="btn btn-circle btn-ghost m-4"
+                onClick={toggleCart}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       )}
